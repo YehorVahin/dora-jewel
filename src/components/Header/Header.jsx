77c1b9e60ae8afb34d7d245dd22b6f71c9b89e82
Yehor Vahin <davyjohns322@gmail.com>
2025-04-28 21:50:57 +0300

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import css from './Header.module.css'
 import { FaTelegram } from "react-icons/fa";
 import React, { Component } from 'react'
 import Categories from '../Categories/Categories';
@@ -8,15 +7,15 @@ import Categories from '../Categories/Categories';
 export default function Header() {
   
     return (
-      <div className={css.header}>
-        <div className={css.logo}><p className={css.textlogo}>DoraJewel</p></div>
+      <div className='header'>
+        <div className='logo'><p className='text-logo'>DoraJewel</p></div>
         <div className='nav-menu'>
         <Categories chooseCategory={this.chooseCategory}/>
         </div>
         
-        <div className={css.telegramlink}>
-                <FaTelegram className={css.telegram}/>
-                <a target='blank' href="https://l.instagram.com/?u=https%3A%2F%2Ft.me%2F%2B3j_naYRgY9k5ODgy%3Ffbclid%3DPAZXh0bgNhZW0CMTEAAacrUuYwm48LQP-tVD_096E8jR5SS00hLDd8wa-SWOnEW6Xf3AjXnwgNZnYkIw_aem_V9JoBQILNVsFvDiHX9aXQQ&e=AT1N2NXhP9AlRblD91sF_SM62XpR_4BDuWKq4qZjiiFslENKm0WiZP6MiHuKlGYS2WKfv5UrQ9mzRB70X3rMUGCTG3EDfNjsl6QhhQ">Телеграм канал</a>
+        <div className='telegram-link'>
+                <FaTelegram className='telegram'/>
+                <a  href="https://l.instagram.com/?u=https%3A%2F%2Ft.me%2F%2B3j_naYRgY9k5ODgy%3Ffbclid%3DPAZXh0bgNhZW0CMTEAAacrUuYwm48LQP-tVD_096E8jR5SS00hLDd8wa-SWOnEW6Xf3AjXnwgNZnYkIw_aem_V9JoBQILNVsFvDiHX9aXQQ&e=AT1N2NXhP9AlRblD91sF_SM62XpR_4BDuWKq4qZjiiFslENKm0WiZP6MiHuKlGYS2WKfv5UrQ9mzRB70X3rMUGCTG3EDfNjsl6QhhQ">Телеграм канал</a>
         </div>
       </div>
     )
