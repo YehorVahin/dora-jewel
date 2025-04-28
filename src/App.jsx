c581292edc10e431e4611cd4a9169ecae49680fd
Yehor Vahin <davyjohns322@gmail.com>
2025-04-28 21:23:10 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -10,86 +10,116 @@ class App extends React.Component {
     this.state =
     {
       currentItems: [],
-      item: [
+      items: [
         {
-          "id": 1,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 1,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       },
       {
-          "id": 2,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 2,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       },
       {
-          "id": 3,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 3,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       },
       {
-          "id": 4,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 4,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       },
       {
-          "id": 5,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 5,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       },
       {
-          "id": 6,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 6,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       },
       {
-          "id": 7,
-          "key": "rings",
-          "name": "Каблучка",
-          "color": "Жовте золото",
-          "stone": "Рубін",
-          "image": "ring.png",
-          "price": "20000"
+          id: 7,
+          key: "rings",
+          name: "Каблучка",
+          color: "Жовте золото",
+          stone: "Рубін",
+          image: "ring.png",
+          price: "20000"
       }
       ],
       showFullItem: false,
       fullItem: {}
     }
     this.state.currentItems = this.state.items
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
     this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
   }
   render(){
   return (
     <>
-     <Header/>
+     <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
      <Presentation/>
-     <Items/>
+     <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder}/>
     </>
   )
 }
+onShowItem(item) {
+  this.setState({fullItem: item})
+  this.setState({showFullItem: !this.state.showFullItem})
+}
+
+chooseCategory(category) {
+  if (category === 'all') {
+    this.setState({currentItems: this.state.items})
+    return
+  }
+  this.setState({currentItems: this.state.items.filter(el => el.category === category)})
+}
+
+deleteOrder(id) {
+  this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+}
+addToOrder(item){
+  let isInArray = false
+  this.state.orders.forEach(el => {
+    if (el.id === item.id) 
+      isInArray = true
+    
+  })
+    if (!isInArray) {
+      this.setState({orders:[...this.state.orders, item]})
+    }
+  }
 }
 
 export default App
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,11 +4,13 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/" + this.props.item.img} alt="" onClick={() => this.props.onShowItem(this.props.item)} />
-        <h2>{this.props.item.title}</h2>
-        <p>{this.props.item.desc}</p>
-        <b>{this.props.item.price}</b>
-        <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+        <img src={"./img/" + this.props.item.img} alt="" onClick={() => this.props.onShowItem(this.props.item)}/>
+        <div className=''>
+            <h2 className=''>{this.props.item.name}</h2>
+            <p className=''>{this.props.item.color}</p>
+            <p className=''>{this.props.item.stone}</p>
+            <b className=''>{this.props.item.price} грн.</b>
+      </div>
       </div>
     )
   }
