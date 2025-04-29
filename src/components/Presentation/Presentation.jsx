da90080f311e0833c5d7310987cc90d357104954
Yehor Vahin <davyjohns322@gmail.com>
2025-04-30 00:52:39 +0300

    changed slider

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -10,7 +10,9 @@ export default function Presentation ()  {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
   }
     return (
       <div className={css.sliderinner}>
