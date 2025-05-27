82e3524d4b27ec431232fb74a8ecca4154490c54
Yehor Vahin <davyjohns322@gmail.com>
2025-05-27 16:59:38 +0300

    Update Items.jsx

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,32 +1,46 @@
-import css from './Items.module.css'
-import Item from '../Item/Item'
-import { Modal } from '../Modal/Modal'
-import {React, useState} from 'react'
+import css from "./Items.module.css";
+import Item from "../Item/Item";
+import { Modal } from "../Modal/Modal";
+import { React, useState } from "react";
 
-
-const Items = ({item}) => {
-    const [modalInfoIsOpen, setModalInfoOpen] = useState(false);
-    return(
-        <main>
-        <ul className={css.items}>
-            {item.map(({id, image, name, weight, stone, article}) => (
-                <li className={css.listitem} key={id}>
-                    <Item image={image} name={name} weight={weight} stone={stone} article={article}>
-                    <button className={css.button_open} onClick={() => setModalInfoOpen(true)}>Детальніше</button>
-                    </Item>
-                </li>
-            ))}
-        </ul>
-        <Modal  isOpen={modalInfoIsOpen} onClose={() => setModalInfoOpen(false)}>
-        {item.map(({id, image, name, weight, stone, article}) => (
-                <div className={css.listitem} key={id}>
-                    <Item image={image} name={name} weight={weight} stone={stone} article={article}>
-                        
-                        </Item>
-                </div>
-            ))}
-        </Modal>
-        </main>
-    )
-}
-export default Items
+const Items = ({ item }) => {
+  const [modalInfoIsOpen, setModalInfoOpen] = useState(false);
+  return (
+    <main>
+      <ul className={css.items}>
+        {item.map(({ id, image, name, weight, stone, article }) => (
+          <li className={css.listitem} key={id}>
+            <Item
+              image={image}
+              name={name}
+              weight={weight}
+              stone={stone}
+              article={article}
+            >
+              <button
+                className={css.button_open}
+                onClick={() => setModalInfoOpen(true)}
+              >
+                Детальніше
+              </button>
+            </Item>
+          </li>
+        ))}
+      </ul>
+      <Modal isOpen={modalInfoIsOpen} onClose={() => setModalInfoOpen(false)}>
+        {item.map(({ id, image, name, weight, stone, article }) => (
+          <div className={css.listitem} key={id}>
+            <Item
+              image={image}
+              name={name}
+              weight={weight}
+              stone={stone}
+              article={article}
+            ></Item>
+          </div>
+        ))}
+      </Modal>
+    </main>
+  );
+};
+export default Items;
